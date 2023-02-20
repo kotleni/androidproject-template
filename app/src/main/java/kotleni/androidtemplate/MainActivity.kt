e447a0c9de4b912f1d81e82bb3fe645728cf4b5d
kotleni <yavarenikya@gmail.com>
2023-02-21 00:57:52 +0200

    Initial

---
+++ app/src/main/java/kotleni/androidtemplate/MainActivity.kt
@@ -0,0 +1,14 @@
+package kotleni.androidtemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotleni.androidtemplate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
